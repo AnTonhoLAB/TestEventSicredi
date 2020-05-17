ed107fc9612b8c279704cfd69012c42df2e9b0d6
George Gomes <georgegomees@gmail.com>
2020-05-17 01:00:17 -0300

    Setup cocoapods to use Mockingjay for stubs

--- TestSicredi/AppDelegate.swift
+++ TestSicredi/AppDelegate.swift
@@ -13,6 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let requester = EventListRequester(nil)
+        requester.getList() { result in
+            // get list and make the tableview
+        }
+        
         return true
     }
 
---
+++ TestSicrediTests/EventListRequesterTests.swift
@@ -0,0 +1,20 @@
+//
+//  EventListRequesterTests.swift
+//  TestSicrediTests
+//
+//  Created by George Gomes on 17/05/20.
+//  Copyright © 2020 George Gomes. All rights reserved.
+//
+
+import XCTest
+import Mockingjay
+
+class EventListRequesterTests: XCTestCase {
+
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
