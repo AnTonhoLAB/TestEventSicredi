8b5235b570fb934f73bebb6b2a94b6ad2cd01540
George Gomes <georgegomees@gmail.com>
2020-05-17 23:00:57 -0300

    Create mvvm pattern files

---
+++ TestSicredi/EventDetailView.swift
@@ -0,0 +1,9 @@
+//
+//  EventDetailView.swift
+//  TestSicredi
+//
+//  Created by George Gomes on 17/05/20.
+//  Copyright © 2020 George Gomes. All rights reserved.
+//
+
+import Foundation
---
+++ TestSicredi/EventDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EventDetailViewController.swift
+//  TestSicredi
+//
+//  Created by George Gomes on 17/05/20.
+//  Copyright © 2020 George Gomes. All rights reserved.
+//
+
+import Foundation
